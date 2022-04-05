0c258335cfd12138f961505fd1cd20f8ff098ee4
mehediaziz <mehediaziz@yahoo.com>
2022-04-05 12:59:39 -0400

    first commit

---
+++ src/main/java/com/jenkins/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.jenkins.test;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Jenkins - Hello World");
+
+    }
+}
